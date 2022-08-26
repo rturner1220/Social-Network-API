b7b2d27f50b0908076696da04f2bb5345a707da6
Rosa Turner <rmturner1220@gmail.com>
2022-08-26 00:21:16 -0400

    routes updated

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction_test');
+const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
 // Thought
